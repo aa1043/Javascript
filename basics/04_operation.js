69da700efa46a4a8e380b8d153ee073ee0e8097f
Aryadeep Das <127377190+aa1043@users.noreply.github.com>
2024-08-08 00:38:22 +0530

    Update 04_operation.js

--- basics/04_operation.js
+++ basics/04_operation.js
@@ -1,23 +1,23 @@
-// let v = 3;
-// let nv = -v;
-// console.log(nv)
-// console.log(3**10)//power 3 to the power 10
-// let str1="Hello"
-// let str2=" Aryadeep"
-// let str3=str1+str2
-// console.log(str3)
-// console.log("1" + 4 + 5)//145
-// console.log("1" + "5")//15
-// console.log(1+4 + "5")//55
-// console.log(+true)//1
-// let x = 3;
-// const y = x++;
+let v = 3;
+let nv = -v;
+console.log(nv)
+console.log(3**10)//power 3 to the power 10
+let str1="Hello"
+let str2=" Aryadeep"
+let str3=str1+str2
+console.log(str3)
+console.log("1" + 4 + 5)//145
+console.log("1" + "5")//15
+console.log(1+4 + "5")//55
+console.log(+true)//1
+let x = 3;
+const y = x++;
 
-// console.log(`x:${x}, y:${y}`);
-// // Expected output: "x:4, y:3"
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
 
-// let a = 3;
-// const b = ++a;
+let a = 3;
+const b = ++a;
 
-// console.log(`a:${a}, b:${b}`);
-// // Expected output: "a:4, b:4"
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
