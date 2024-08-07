ceedf6104ccd03617788d58b6507a7850c54d7c6
Aryadeep Das <127377190+aa1043@users.noreply.github.com>
2024-08-07 19:27:13 +0000

    check

---
+++ basics/05_comparison.js
@@ -0,0 +1,13 @@
+console.log(3>1)
+console.log(3>=1)
+console.log(3==1)
+console.log("3">1)//also returns true
+console.log("3">1)//also returns true
+console.log(null>0)//false
+console.log(null>=0)//true as = converts null to 0 
+console.log(undefined==0);//false as it dose not give a shit
+console.log(undefined>=0);//false as it dose not give a shit
+
+// Strict check ===
+console.log("9"===9);//false as strict check
+
