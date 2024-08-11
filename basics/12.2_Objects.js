daeb78543bc682b1031e433abde18a619bb8cc48
Aryadeep Das <127377190+aa1043@users.noreply.github.com>
2024-08-12 05:23:31 +0530

    Add files via upload

---
+++ basics/12.2_Objects.js
@@ -0,0 +1,41 @@
+
+//                               Object de-structure and JSON
+
+const course=
+{
+  coursename:"JavaScript",
+  courseInstructor:"Hitesh Sir",
+  price:"Free",
+  platform:"Youtube"
+}
+// SYNTAX YOU SHOULD FOLLOW LIKE IT HELPS IN ACCESINT THE ENTITY AND ALSO RENAME IT
+const {courseInstructor:Instructor} =course
+//console.log(courseInstructor);//Hitesh Sir
+console.log(Instructor);//Hitesh Sir
+
+
+//          THIS IS NOT JAVASCRIPT THIS IS REACT
+
+// const navbar = ({COMPANY}) => 
+//   {
+
+//   } 
+
+//                API INTRO
+
+//JSON
+
+// {
+//   "coursename":"JavaScript",
+//   "courseInstructor":"Hitesh Sir",
+//   "price":"Free",
+//   "platform":"Youtube"
+// }
+
+/*
+[
+   {}
+   {}
+   {}
+]
+   */
