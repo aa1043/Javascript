2994f377047182d39f511188c61c2f1ca62edbc6
Aryadeep Das <127377190+aa1043@users.noreply.github.com>
2024-08-17 14:12:12 +0530

    Add files via upload

---
+++ basics/18_WhileLoop.js
@@ -0,0 +1,14 @@
+// let index=0
+// while (index<10)
+// {
+//   console.log(index);
+//   index++
+// }
+
+// const arr=new Array(3,6,3,2,1,8,5)
+// let ind=0;
+// while(ind<arr.length)
+// {
+//   console.log(arr[ind]);
+//   ind++;
+// }
---
+++ basics/19_DoWhileLoop.js
@@ -0,0 +1,18 @@
+let score =20
+do 
+{
+  console.log(score);
+  score++
+} 
+while (score<30);
+
+// 20
+// 21
+// 22
+// 23
+// 24
+// 25
+// 26
+// 27
+// 28
+// 29
